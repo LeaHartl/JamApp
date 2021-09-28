3cf25865ac7c8f0aa257240356e9694d5c6241f4
LeaHartl <lea.hartl@powderguide.com>
2021-09-28 20:34:44 +0200

    testing postgres

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Gletscher_Eis'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'stakes10.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'stakes10.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 

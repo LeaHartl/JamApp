6c7c15c780709580ba2eb7db15aac0bf09f9025a
LeaHartl <lea.hartl@powderguide.com>
2021-09-28 20:40:23 +0200

    trying things

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Gletscher_Eis'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'stakes10.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'stakes10.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+    #     'postgres://', 'postgresql://') or \
+    #     'sqlite:///' + os.path.join(basedir, 'stakes10.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
